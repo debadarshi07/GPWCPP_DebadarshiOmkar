fbc90ff2ee369a2af3977fa27e3b2e2bdf22c01a
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-01-27 23:05:06 +0530

    OOPS in C++

---
+++ Practice/OOPS/Test_05.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+public:
+    string name;
+    int age;
+
+    Person(string name, int age) {
+        this -> name = name;
+        this -> age = age;
+    }
+};
+
+class Student : public Person {
+public:
+    string standard;
+    int section;
+
+    Student(string name, int age, string standard, int section) : Person(name, age) {
+        this -> standard = standard;
+        this -> section = section;
+    }
+};
+
+class GraduationStudent : public Student {
+public:
+    string researchField;
+    GraduationStudent(string name, int age, string standard, int section, string researchField) : Student(name, age, standard, section) {
+        this -> researchField = researchField;
+    }
+
+    
+    void getStudentInformation() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Standard: " << standard << endl;
+        cout << "Section: " << section << endl;
+        cout << "Research Field: " << researchField << "\n" << endl;
+    }
+};
+
+int main() {
+    GraduationStudent student1("Debadarshi Omkar", 21, "Graduation", 41028, "Natural Language Processing");
+    student1.getStudentInformation();
+
+    GraduationStudent student2("Pat Cummins", 35, "Cricketer", 87659, "Crowd Silencing");
+    student2.getStudentInformation();
+}
