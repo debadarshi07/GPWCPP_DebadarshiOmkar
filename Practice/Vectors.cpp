ddf92169ca5a659ea3f1cfd2f78f7975dbbab71a
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-06-18 23:45:30 +0530

    Practice

--- Practice/Vectors.cpp
+++ Practice/Vectors.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 int main() {
-    vector<int> vec = {1,2,3,4,5};
-    cout << vec[0] << endl;
+    vector<int> vec1 = {1, 2, 3, 4, 5};
+    cout << vec1[0] << endl;
+
+    vector<int> vec2(5, 2);
+    for (int i = 0; i < 5; i++) {
+        cout << vec2[i] << "\t";
+    }
+
+
+
     return 0;
 }
